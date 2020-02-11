4d2326c39b9f9a9e75e93a9e496aaf95efeefd7c
Bernardo Simonassi Moura <36281298+BernardoSM@users.noreply.github.com>
2020-02-11 10:41:21 -0300

    Add files via upload

---
+++ Exemplo01.ts
@@ -0,0 +1,7 @@
+function saudacao(pessoa){
+	return "Olá " + pessoa;
+} 
+
+var alguem = "Alunos da Sala de Aula 222222";
+
+console.log(saudacao(alguem));
---
+++ out/Exemplo01.js
@@ -0,0 +1,6 @@
+function saudacao(pessoa) {
+    return "Olá " + pessoa;
+}
+var alguem = "Alunos da Sala de Aula 222222";
+console.log(saudacao(alguem));
+//# sourceMappingURL=Exemplo01.js.map
